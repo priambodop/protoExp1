4d8b07efbad3b18deb327db845c87a8c9a7d4dd5
priambodop <priambodop>
2018-03-22 17:29:34 +0700

    able to join the player with a specific namespace

--- public/javascripts/assemblyScript.js
+++ public/javascripts/assemblyScript.js
@@ -1,19 +1,36 @@
-$(function(){
-  var play1 = document.getElementById("p1");
-  var play2 = document.getElementById("p2");
-  var player = 0;
-  var socket = io();
+var play1 = document.getElementById("p1");
+var play2 = document.getElementById("p2");
+var number = "";
+var player = 0;
+var socket = io();
 
-  if (player === 0) {
-    socket.on('player joining', function(msg){
+
+function getRandInt(){
+  var text = "";
+  text = Math.floor(Math.random() * (999999 - 111111)) + 111111;
+  
+  number = text;
+
+  document.getElementById("code").innerHTML = text;
+}
+
+window.onload = getRandInt;
+
+
+socket.on('player joining', function(msg){
+
+  if (msg == number) {
+    if (player < 1) {
       play1.innerHTML = 'player is joining in ' + msg;
       socket.emit('you join', 'YES YOU ARE JOINED !!!');
-    });
-    player += 1;
-  }else{
-    socket.on('player join', (msg) => {
-      play1.innerHTML = msg;
-    });
-    player += 1;
+      player = player + 1;
+    }else {
+      play2.innerHTML = 'player is joining in ' + msg;
+      socket.emit('you join', 'YES YOU ARE JOINED !!!');
+      player = player + 1;
+    }
+  }else {
+    socket.emit('wrong code', 'You are not permitted !');
   }
+
 });
